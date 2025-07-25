99192e18f0b5f55a84657e02f0b0f9993961788a
Levent C. <vrkafa@gmail.com>
2025-07-26 00:00:58 +0300

    hardhat config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@fhevm/hardhat-plugin";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
   networks: {
-    zama: {
-      url: "https://sepolia.fhe.ethereum.org",
-      chainId: 11155420,
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 11155111,
       accounts: [process.env.PRIVATE_KEY!],
     },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v6",
   },
 };
 
