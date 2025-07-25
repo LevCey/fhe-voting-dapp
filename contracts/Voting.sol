18df2fe825fc689c458828b07d3dbd16cfa8653e
Levent C. <vrkafa@gmail.com>
2025-07-25 22:59:14 +0300

    votting

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,23 +2,19 @@
 pragma solidity ^0.8.20;
 
 import "@fhenixprotocol/contracts/FHE.sol";
-import "@fhenixprotocol/contracts/FHEUser.sol";
 
-contract Voting is FHEUser {
+contract Voting {
     // Şifreli toplam oylar
     euint32 private yesVotes;
     euint32 private noVotes;
 
-    // Oy kullananları takip etmek için
     mapping(address => bool) public hasVoted;
 
-    // Oy kullanma fonksiyonu
-    function vote(ebool memory encryptedVote) public {
-        require(!hasVoted[msg.sender], "You have already voted.");
+    function vote(ebool encryptedVote) public {
+        require(!hasVoted[msg.sender], "Already voted");
 
-        // encryptedVote == true ise yesVotes++; değilse noVotes++;
-        ebool memory isYes = encryptedVote;
-        ebool memory isNo = FHE.not(encryptedVote);
+        ebool isYes = encryptedVote;
+        ebool isNo = FHE.not(encryptedVote);
 
         yesVotes = FHE.add(yesVotes, FHE.asEuint32(isYes));
         noVotes = FHE.add(noVotes, FHE.asEuint32(isNo));
@@ -26,12 +22,11 @@ contract Voting is FHEUser {
         hasVoted[msg.sender] = true;
     }
 
-    // Şifreli oyları getirme (sadece frontend decrypt edebilir)
-    function getEncryptedYesVotes() public view returns (euint32 memory) {
+    function getEncryptedYesVotes() public view returns (euint32) {
         return yesVotes;
     }
 
-    function getEncryptedNoVotes() public view returns (euint32 memory) {
+    function getEncryptedNoVotes() public view returns (euint32) {
         return noVotes;
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    // deploy sırasında kullanılacak (şimdilik boş bırakabilirsin)
+  },
 };
 
 export default config;
