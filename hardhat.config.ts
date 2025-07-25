b2a6ae6a32517e304ae15ec0ed11dfe4570ffb31
Levent C. <vrkafa@gmail.com>
2025-07-26 00:25:21 +0300

    hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
+import "@fhevm/hardhat-plugin";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: {
     version: "0.8.24",
     settings: {
-      optimizer: {
-        enabled: true,
-        runs: 800,
-      },
+      optimizer: { enabled: true, runs: 800 },
     },
   },
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 11155111,
-      accounts: [process.env.PRIVATE_KEY!],
+      accounts: {
+        mnemonic: process.env.MNEMONIC!,
+        path: "m/44'/60'/0'/0/",
+        count: 10,
+      },
     },
   },
   typechain: {
